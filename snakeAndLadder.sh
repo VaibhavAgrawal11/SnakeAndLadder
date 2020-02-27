b192a0f0bf55930a76d70cd407e0b2f1283fb24c
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-27 12:16:29 +0530

    MODIFIED: Added wining condition

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,30 +1,35 @@
 #!/bin/bash -x
-printf "Check for the option to play next \n"
+printf "Added wining condition \n"
 #VARIABLES
 numberOfPlayers=1
 position=0
-rollTheDie=$((RANDOM%6+1))
-optionCheck=$((RANDOM%3))
-#CHECK OPTIONS NOPLAY/LADDER/SNAKE
-case $optionCheck in
-	0)
-	printf "No play\n"
-	position=$position
-	printf "$position"
-	;;
-	1)
-	printf "Yeahh Ladder!!\n"
-	position=$((position+rollTheDie))
-	printf "$position"
-	;;
-	2)
-	printf "Opps snake..\n"
-	if(($position>$rollTheDie)) 
-	then
-		position=$((position-rollTheDie))
-	else
+
+#PLAY TILL 100
+while(($position<100))
+do
+	rollTheDie=$((RANDOM%6+1))
+	optionCheck=$((RANDOM%3))
+	#CHECK OPTIONS NOPLAY/LADDER/SNAKE
+	case $optionCheck in
+		0)
+		printf "No play\n"
 		position=$position
-	fi
-	printf "$position"
-   ;;
-esac
+		printf "$position"
+		;;
+		1)
+		printf "Yeahh Ladder!!\n"
+		position=$((position+rollTheDie))
+		printf "$position"
+		;;
+		2)
+		printf "Opps snake..\n"
+		if(($position>$rollTheDie)) 
+		then
+			position=$((position-rollTheDie))
+		else
+			position=0
+		fi
+		printf "$position"
+   	;;
+	esac
+done
