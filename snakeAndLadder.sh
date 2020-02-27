c7fda57f97c0a3d637b9b9e1cb556c45dbce280e
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-27 11:59:08 +0530

    MODIFIED: Added feature to check options to play next

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,6 +1,30 @@
 #!/bin/bash -x
-printf "Roll the die to generate 1 to 6 \n"
+printf "Check for the option to play next \n"
 #VARIABLES
 numberOfPlayers=1
 position=0
 rollTheDie=$((RANDOM%6+1))
+optionCheck=$((RANDOM%3))
+#CHECK OPTIONS NOPLAY/LADDER/SNAKE
+case $optionCheck in
+	0)
+	printf "No play\n"
+	position=$position
+	printf "$position"
+	;;
+	1)
+	printf "Yeahh Ladder!!\n"
+	position=$((position+rollTheDie))
+	printf "$position"
+	;;
+	2)
+	printf "Opps snake..\n"
+	if(($position>$rollTheDie)) 
+	then
+		position=$((position-rollTheDie))
+	else
+		position=$position
+	fi
+	printf "$position"
+   ;;
+esac
